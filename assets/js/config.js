f23ab21bc5db801aee78858c00977935716c9715
Muhammad Awais <175257405+poorman718@users.noreply.github.com>
2026-05-23 03:50:02 +0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_KEY: 'YOUR_RAPIDAPI_KEY',   // 👈 Replace with your key
+    API_KEY: '23d7792dfamshac4ad4751a68f49p1438c8jsn17a353c04fbd',   // 👈 Replace with your key
     API_HOST: 'instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com',
     API_BASE_URL: 'https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/'
 };
